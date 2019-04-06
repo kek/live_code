17b44295963ee39bb10d764ce3c9e53503d6bda0
Karl Eklund <localpart@gmail.com>
2019-04-06 19:15:55 +0200

    Add map function

--- lib/live_code/library.ex
+++ lib/live_code/library.ex
@@ -1,5 +1,6 @@
 defmodule LiveCode.Library do
   use Symbelix.Library
+  require Logger
 
   def add(a, b), do: a + b
 
@@ -8,4 +9,18 @@ defmodule LiveCode.Library do
   end
 
   def eq(left, right), do: left == right
+
+  def map(list, function) do
+    list
+    |> Enum.map(fn item ->
+      {:ok, ast} = generate_ast([function, item])
+      Logger.info(inspect(ast))
+      {result, _binding} = Code.eval_quoted(ast)
+      result
+    end)
+  end
+
+  def id(x), do: x
+
+  def test, do: "test"
 end
